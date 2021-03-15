4d92c30788a95d17cefa843af7293fc2c1c89323
OT4EHAW <78928902+OT4EHAW@users.noreply.github.com>
2021-03-16 00:24:37 +0300

    Update task5_for.cpp

--- task5/task5_for/task5_for.cpp
+++ task5/task5_for/task5_for.cpp
@@ -7,7 +7,7 @@ int k;
 int findFirstNegativeElement(double eps) {
 	s = 0;
 	for (int i = 0; ; i++) {
-		s = pow(-1, i) * (pow(2, i) / (pow(i, i + 1) + 1));
+		s = pow(-1, i) * (1-(2 * i) / (2 * i + 1));
 		if (s > 0) continue;
 		k = i;
 		if (abs(s) < eps) break;
