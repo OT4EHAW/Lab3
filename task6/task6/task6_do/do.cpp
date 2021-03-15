1af43eda607907921dd480e7e640a9cba7d56f2e
OT4EHAW <78928902+OT4EHAW@users.noreply.github.com>
2021-03-16 00:25:39 +0300

    Update do.cpp

--- task6/task6/task6_do/do.cpp
+++ task6/task6/task6_do/do.cpp
@@ -5,7 +5,7 @@ double sum(int n) {
     double sum1 = 0;
     int i = 0;
     do {
-        sum1 += pow(-1, i) * (pow(2, i) / (pow(i, i + 1) + 1));
+        sum1 += pow(-1, i) * (1-(2 * i) / (2 * i + 1));
         i++;
     } while (i < n);
     return sum1;
@@ -16,7 +16,7 @@ double sum2(double eps) {
     double s = 0;
     double a = 0;
     do {
-        a = pow(-1, i) * (pow(2, i) / (pow(i, i + 1) + 1));
+        a = pow(-1, i) * (1-(2 * i) / (2 * i + 1));
         s += a;
         i++;
     } while (abs(a) >= eps);
@@ -28,7 +28,7 @@ void print(int n, int k) {
     int i = 0;
     double sum1 = 0;
     do {
-        sum1 = pow(-1, i) * (pow(2, i) / (pow(i, i + 1) + 1));
+        sum1 = pow(-1, i) * (1-(2 * i) / (2 * i + 1));
         i++;
         if (i % k == 0) continue;
         cout << sum1 << endl;
@@ -40,7 +40,7 @@ int findFirstElement(double eps) {
     int k = 0;
     double sum1 = 0;
     do {
-        sum1 = pow(-1, i) * (pow(2, i) / (pow(i, i + 1) + 1));
+        sum1 = pow(-1, i) * (1-(2 * i) / (2 * i + 1));
         k = i;
         i++;
     } while (abs(sum1) >= eps);
@@ -50,10 +50,10 @@ int findFirstElement(double eps) {
 int findFirstNegativeElement(double eps) {
     int i = 0;
     int k = 0;
-    double sum1 = pow(-1, i) * (pow(2, i) / (pow(i, i + 1) + 1));
+    double sum1 = pow(-1, i) * (1-(2 * i) / (2 * i + 1));
     do {
         i++;
-        sum1 = pow(-1, i) * (pow(2, i) / (pow(i, i + 1) + 1));
+        sum1 = pow(-1, i) * (1-(2 * i) / (2 * i + 1));
         if (sum1 > 0) continue;
         k = i;
     } while (abs(sum1) >= eps);
